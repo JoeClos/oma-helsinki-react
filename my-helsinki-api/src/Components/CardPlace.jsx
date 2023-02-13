86628f67c36bffb3758934ca2556da1794f95819
Josephine Closan <89244648+JoeClos@users.noreply.github.com>
2023-02-13 19:13:58 +0200

    create a new component for cards

---
+++ my-helsinki-api/src/Components/CardPlace.jsx
@@ -0,0 +1,108 @@
+import { useState } from "react";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import Collapse from "@mui/material/Collapse";
+import Avatar from "@mui/material/Avatar";
+import { green } from "@mui/material/colors";
+import CardActions from "@mui/material/CardActions";
+import IconButton from "@mui/material/IconButton";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ShareIcon from "@mui/icons-material/Share";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import CardMedia from "@mui/material/CardMedia";
+import { styled } from "@mui/material/styles";
+
+
+const ExpandMore = styled((props) => {
+    const { expand, ...other } = props;
+    return <IconButton {...other} />;
+  })(({ theme, expand }) => ({
+    transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
+    marginLeft: "auto",
+    transition: theme.transitions.create("transform", {
+      duration: theme.transitions.duration.shortest,
+    }),
+  }));
+  
+
+const CardPlace = ({place}) => {
+    const [expanded, setExpanded] = useState(false);
+    const handleExpandClick = () => {
+        setExpanded({
+          zoom: true,
+          // selectedCard: id,
+        });
+      };
+    
+
+  return (
+      <Card sx={{ maxWidth: 345 }}>
+        <CardHeader
+          avatar={
+            <Avatar sx={{ bgcolor: green[500] }} aria-label="recipe">
+              {place.name.fi.charAt(0)}
+            </Avatar>
+          }
+          title={place.name.en}
+          subheader={
+            <a
+              href={place.info_url}
+              target="_blank"
+              rel="noreferrer"
+              style={{ textDecoration: "none" }}
+            >
+              Link
+            </a>
+          }
+        />
+
+        <CardContent>
+          <Typography variant="body2" color="text.secondary">
+            {place.description.intro}
+          </Typography>
+        </CardContent>
+        <CardActions disableSpacing>
+          <IconButton aria-label="add to favorites">
+            <FavoriteIcon />
+          </IconButton>
+          <IconButton aria-label="share">
+            <ShareIcon />
+          </IconButton>
+          <ExpandMore
+            expand={expanded}
+            onClick={handleExpandClick}
+            aria-expanded={expanded}
+            aria-label="show more"
+          >
+            <ExpandMoreIcon />
+          </ExpandMore>
+        </CardActions>
+        <Collapse in={expanded} timeout="auto" unmountOnExit>
+          <CardContent>
+            {place.description.images.map((img, index) => (
+              <CardMedia
+                key={index}
+                component="img"
+                height="194"
+                image={img.url}
+                alt="Place description"
+                style={{ padding: "5px" }}
+              />
+            ))}
+            <Typography paragraph>
+              Address:
+              {place.description.body}
+            </Typography>
+            <Typography>{place.location.address.street_address}</Typography>
+            <Typography>{place.location.address.postal_code}</Typography>
+            <Typography>{place.location.address.locality}</Typography>
+          </CardContent>
+        </Collapse>
+        <Collapse out={expanded}></Collapse>
+      </Card>
+  );
+};
+
+export default CardPlace;
