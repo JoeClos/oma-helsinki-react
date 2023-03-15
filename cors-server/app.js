98b8b8974e8b6dc45f07c221a3576b3e8fcc1075
Josephine Closan <89244648+JoeClos@users.noreply.github.com>
2023-03-15 10:56:32 +0200

    format doc with prettier

--- cors-server/app.js
+++ cors-server/app.js
@@ -1,45 +1,40 @@
-// import * as dotenv from "dotenv"
-require('dotenv').config()
+require("dotenv").config();
 
 const axios = require("axios");
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const mongoose = require('mongoose');
-const UserModel = require('./models/Users')
+const mongoose = require("mongoose");
+const UserModel = require("./models/Users");
 
 const URL = process.env.DB_URL;
 const port = process.env.PORT || 8000;
-app.use(express.json())
+app.use(express.json());
 app.use(cors());
 
-// mongoose.connect('mongodb+srv://USER:PASSWORD@cluster0.agps2lc.mongodb.net/userDB?retryWrites=true&w=majority')
-mongoose.connect(URL)
-
-
-
+mongoose.connect(URL);
 
 app.get("/", (req, res) => {
   res.send("Welcome to CORS server 😁");
 });
 
 app.get("/getUsers", async (req, res) => {
-    const users = await UserModel.find();
-    console.log(users);
-    res.json(users)
+  const users = await UserModel.find();
+  console.log(users);
+  res.json(users);
 });
 
 app.post("/createUser", async (req, res) => {
-  const user = req.body
+  const user = req.body;
   const newUser = new UserModel(user);
   await newUser.save();
 
   res.json(user);
-})
+});
 
 app.get("/v2/places", (request, response) => {
   response.header({ "Access-Control-Allow-Origin": "*" });
-  response.header({'Cache-control': 'public, max-age=300'})
+  response.header({ "Cache-control": "public, max-age=300" });
   axios
     .get("https://open-api.myhelsinki.fi/v2/places/")
     .then((res) => {
@@ -51,21 +46,21 @@ app.get("/v2/places", (request, response) => {
     });
 });
 app.get("/v2/place/:id", (request, response) => {
-  response.header({"Access-Control-Allow-Origin": "*" });
-  response.header({'Cache-control' : 'public, max-age=300'});
+  response.header({ "Access-Control-Allow-Origin": "*" });
+  response.header({ "Cache-control": "public, max-age=300" });
 
   axios
-  .get(`https://open-api.myhelsinki.fi/v2/place/${Number(request.params.id)}`)
-  .then((res) => {
-    response.send(res.data);
-  })
-  .catch((err) => {
-    response.status(400);
-    response.send(err.config.data || "Couldn't fetch data!");
-    console.log(err);
-  });
-  console.log(request.params)
-})
+    .get(`https://open-api.myhelsinki.fi/v2/place/${Number(request.params.id)}`)
+    .then((res) => {
+      response.send(res.data);
+    })
+    .catch((err) => {
+      response.status(400);
+      response.send(err.config.data || "Couldn't fetch data!");
+      console.log(err);
+    });
+  console.log(request.params);
+});
 app.listen(port, () => {
   console.log(`Now listening on port ${port}...`);
 });
